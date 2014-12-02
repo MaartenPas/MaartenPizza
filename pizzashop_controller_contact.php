c8ea7527628f7fe57e01ad8bc2eccff6819deb1e
MaartenPas <mppasschijn@hotmail.com>
2014-12-02 16:03:37 +0100

    maandagnamiddag

--- pizzashop_controller_contact.php
+++ pizzashop_controller_contact.php
@@ -6,4 +6,5 @@ include_once("/presentation/pizzashop_header.php");
 include_once("/presentation/pizzashop_contact_content.php");
 include_once("/presentation/pizzashop_footer.php");
 ?>
+<!--contact-->
 
